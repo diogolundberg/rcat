0207b6df61358e075f8e89e2e30250591c108562
Diogo Lundberg <desenvolvimento1@fencom.com.br>
2015-11-19 14:43:42 -0200

    Adicionado exemplo de coneção do R com o sql server.

--- R/Candidatox_proximo item.R
+++ R/Candidatox_proximo item.R
@@ -1,38 +1,38 @@
 rm(list=ls(all=T))
 
-#Carregando o pacote 
+#Carregando o pacote
 library(mirtCAT)
 
-Dados <- read.table("Banco_500Itens.txt",header=T)
-attach(Dados)
+#Parametros retirados do banco de dados
+connection <- odbcDriverConnect('driver={SQL Server};server=localhost;database=SCAT;trusted_connection=true')
+parameters <- sqlQuery(connection , 'select Question, parametro_a, parametro_d, parametro_g, from TbMirtCat')
+choices <- sqlQuery(connection , 'select Option1, Option2, Option3, Option4, Option5 from TbMirtCat')
+attach(parameters)
 
-#Parametros utilizados no modelo 
-a <- Parametro_a	# parâmetro de discriminação do item 
-d <- Parametro_d	#Reparametrização 
-g <- Parametro_c  #Parametro de acerto casual 
-
-#Opções de resposta #nesse caso são 5 alternativas
-choices <- subset(Dados[,7:11],colnames=F)
+#Parametros utilizados no modelo
+a <- parametro_a    # parâmetro de discriminação do item
+d <- parametro_d    #Reparametrização
+g <- parametro_g  #Parametro de acerto casual
 
 pars <- data.frame(a1=a, d=d,g=g)
 mod <- generate.mirt_object(pars, '3PL')
 coef(mod,simplify=T)
 
-#Criando um data.frame com as perguntas, alternativas e a resposta correta . 
+#Criando um data.frame com as perguntas, alternativas e a resposta correta .
 df <- data.frame(Question=Question,choices,Type = 'radio',Answer=Answer,stringsAsFactors = FALSE)
 
 #################################################################################################################
-						#Função para encontrar o próximo item a ser respondido   
-#Uma função que retorna o próximo item no teste adaptativo informatizado. 
+                        #Função para encontrar o próximo item a ser respondido
+#Uma função que retorna o próximo item no teste adaptativo informatizado.
 #Deve ser utilizado em conjunto com a função updateDesign.
 #################################################################################################################
- 
+
 Candidatox <- mirtCAT(df, mod, criteria = 'MI',start_item = 'MI',method='EAP',design_elements = T,theta_range= c(-4,4),quadpts=20,design = list(theta_range= c(-3,3),thetas.start=0))
 
-#Informar quais foram as primeiras perguntas respondidas e se estão certas ou erradas 
-Candidatox <- updateDesign(CATdesign, items = c(x,x,x), responses = c(0,0,0))
+#Informar quais foram as primeiras perguntas respondidas e se estão certas ou erradas
+Candidatox <- updateDesign(Candidatox, items = c(x,x,x), responses = c(0,0,0))
 
-#Determinando Theta inicial 
+#Determinando Theta inicial
 Candidatox$person$Update.thetas(Candidatox$design, Candidatox$test)
 
 #Processo iterativo
@@ -40,7 +40,7 @@ Candidatox$person$Update.thetas(Candidatox$design, Candidatox$test)
 findNextItem(Candidatox)
 
 #Pergunta e resposta do candidato
-Candidatox <- updateDesign(CATdesign, items = c(211), responses = c(0))
+Candidatox <- updateDesign(Candidatox, items = c(211), responses = c(0))
 
-#Determinando os outros thetas 
+#Determinando os outros thetas
 Candidatox$person$Update.thetas(Candidatox$design, Candidatox$test)
---
+++ sql/create.sql
@@ -0,0 +1,14 @@
+create table TbMirtCat (
+paramatro_a decimal(4,3),
+paramatro_d decimal(4,3),
+paramatro_g decimal(4,3),
+Question varchar(255),
+Option1 int,
+Option2 int,
+Option3 int,
+Option4 int,
+Option5 int
+);
+
+
+
