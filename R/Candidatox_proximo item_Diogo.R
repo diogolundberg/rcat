109a0da91a314e9399d6dc29d608b0588751f031
Diogo Lundberg <diogo@lundberg.com.br>
2015-11-20 18:29:54 -0200

    Adicionando acesso ao banco remoto.

--- R/Candidatox_proximo item_Diogo.R
+++ R/Candidatox_proximo item_Diogo.R
@@ -1,11 +1,12 @@
 rm(list=ls(all=T))
 
 #Carregando o pacote
-library(mirtCAT)
+if(!require(RODBC)){install.packages("RODBC"); library(RODBC)}
+if(!require(mirtCAT)){install.packages("mirtCAT"); library(mirtCAT)}
 
 #Parametros retirados do banco de dados
-connection <- odbcDriverConnect('driver={SQL Server};server=localhost;database=SCAT;trusted_connection=true')
-parameters <- sqlQuery(connection , 'select Question, parametro_a, parametro_d, parametro_g, from TbMirtCat')
+connection <- odbcConnect(dsn="SCAT")
+parameters <- sqlQuery(connection , 'select Question, parametro_a, parametro_d, parametro_g from TbMirtCat')
 choices <- sqlQuery(connection , 'select Option1, Option2, Option3, Option4, Option5 from TbMirtCat')
 attach(parameters)
 
@@ -19,7 +20,7 @@ mod <- generate.mirt_object(pars, '3PL')
 coef(mod,simplify=T)
 
 #Criando um data.frame com as perguntas, alternativas e a resposta correta .
-df <- data.frame(Question=Question,choices,Type = 'radio',Answer=Answer,stringsAsFactors = FALSE)
+df <- data.frame(Question=Question,choices,Type = 'radio',stringsAsFactors = FALSE)
 
 #################################################################################################################
                         #Função para encontrar o próximo item a ser respondido
@@ -27,21 +28,16 @@ df <- data.frame(Question=Question,choices,Type = 'radio',Answer=Answer,stringsA
 #Deve ser utilizado em conjunto com a função updateDesign.
 #################################################################################################################
 
-Candidatox <- mirtCAT(df, mod, criteria = 'MI',start_item = 'MI',method='EAP',design_elements = T,theta_range= c(-4,4),quadpts=20,design = list(theta_range= c(-3,3),thetas.start=0))
+Candidato <- mirtCAT(df, mod, criteria = 'MI',start_item = 'MI',method='EAP',design_elements = T,theta_range= c(-4,4),quadpts=20,design = list(theta_range= c(-3,3),thetas.start=0))
 
 #Informar quais foram as primeiras perguntas respondidas e se estão certas ou erradas
-Candidatox <- updateDesign(Candidatox, items = c(x,x,x), responses = c(0,0,0))
+Candidato <- updateDesign(Candidato, items = c(1,2,3), responses = c(1,0,0))
 
-#Determinando Theta inicial
-Candidatox$person$Update.thetas(Candidatox$design, Candidatox$test)
-Teta_Estimado <- Candidatox$person$thetas
+#Determinando Theta 
+Candidato$person$Update.thetas(Candidato$design,Candidato$test)
+
+Teta_Estimado <- Candidato$person$thetas
 
 #Processo iterativo
 #Pelo Critério de Máxima Informação encontra o proximo item a ser respodido por esse candidato.
-findNextItem(Candidatox)
-
-#Pergunta e resposta do candidato
-Candidatox <- updateDesign(Candidatox, items = c(211), responses = c(0))
-
-#Determinando os outros thetas
-Candidatox$person$Update.thetas(Candidatox$design, Candidatox$test)
+findNextItem(Candidato)
--- R/updateCandidate.R
+++ R/updateCandidate.R
@@ -1,6 +1,7 @@
-
+﻿
 #Informar quais foram as primeiras perguntas respondidas e se estão certas ou erradas 
 Candidate <- updateDesign(Candidate, items = c(x,x,x), responses = c(0,0,0))
 
 #Determinando Theta inicial 
-teta <- Candidate$person$Update.thetas(Candidate$design, Candidate$test)
+Candidate$person$Update.thetas(Candidate$design, Candidate$test)
+Teta_Estimado <- Candidate$person$thetas
